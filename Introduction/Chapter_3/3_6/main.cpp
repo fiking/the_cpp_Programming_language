11b347ee21585bdd9ba28c25481840b85b7f7f5d
fiking <1129277432@qq.com>
2022-09-25 23:40:43 +0800

    [Introduction][Chapter3] finish input example

---
+++ Introduction/Chapter_3/3_6/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void f() {
+    int i;
+    cin >> i;
+
+    double d;
+    cin >> d;
+}
+
+int main() {
+    const float factor = 2.54;
+    float x, in, cm;
+    char ch = 0;
+
+    cout << "enter length: ";
+
+    cin >> x;
+    cin >> ch;
+
+    switch (ch) {
+        case 'i':
+            in = x;
+            cm = x * factor;
+            break;
+        case 'c':
+            in =  x / factor;
+            cm = x;
+            break;
+        default:
+            in = cm = 0;
+            break;
+    }
+    cout << in << "in = " << cm << "cm\n";
+    return 0;
+}
