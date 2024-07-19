2ff03875ff1a5d625657379f35e76d64c62360e7
fiking <1129277432@qq.com>
2024-07-19 23:59:53 +0800

    [Introduction][Chapter3] finish iterator examples

--- Introduction/Chapter_3/3_8/main.cpp
+++ Introduction/Chapter_3/3_8/main.cpp
@@ -120,7 +120,38 @@ int main_383_4() {
     istream_iterator<string> ii;
     istream_iterator<string> eos;
     for_each(ii, eos, record);
-    for_each(ii, eos, eos);
+    for_each(histogram.begin(), histogram.end(), print);
+    return 0;
+}
+
+bool gt_42(const pair<const string, int> &r) {
+    return r.second > 42;
+}
+
+void f(map<string, int> &m) {
+    typedef map<string, int>::const_iterator MI;
+    MI i = find_if(m.begin(), m.end(), gt_42);
+}
+
+void g(const map<string, int> &m) {
+    int c42 = count_if(m.begin(), m.end(), gt_42);
+}
+
+class Shape {
+public:
+    void draw() {}
+};
+
+void draw(Shape *p) {
+    p->draw();
+}
+
+void fpointer(list<Shape*> &sh) {
+    for_each(sh.begin(), sh.end(), draw);
+}
+
+void gpointer(list<Shape*> &sh) {
+    for_each(sh.begin(), sh.end(), mem_fun(&Shape::draw));
 }
 
 int main() {
